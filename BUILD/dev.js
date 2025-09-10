ef955923d23586e8fd3c735922114d5d89010643
Eric <info@ericparlier.com>
2025-09-10 17:41:17 +0200

    build

--- BUILD/dev.js
+++ BUILD/dev.js
@@ -1,1 +1,23 @@
 /* BUILD FILE */
+const fs = require('fs');
+const UglifyJS = require('uglify-js');
+const files = [
+     'SRC/list.js', 
+     'SRC/dom.js', 
+     'SRC/data.js', 
+     'SRC/oml.js', 
+     'SRC/ose.js', 
+     'SRC/object.js', 
+     'SRC/display.js', 
+     'SRC/uxi.js', 
+     'SRC/touch.js', 
+     'SRC/touches.js', 
+     'SRC/events.js', 
+     'SRC/net.js', 
+     'SRC/stage.js', 
+     'SRC/view.js', 
+     'SRC/app.js'
+];
+const mergedCode = files.map((file) => fs.readFileSync(file, 'utf8')).join('\n');
+const result = UglifyJS.minify(mergedCode);
+fs.writeFileSync('DIST/ogx.dev.min.js', result.code);
